6945e88a5889eebeac782cd24f85ad9d083b979b
Deepak <d.porwal06@gmail.com>
2023-12-15 18:26:54 +0530

    Error fix for getting routerdata by id

--- frontend/src/components/EventItem.js
+++ frontend/src/components/EventItem.js
@@ -1,10 +1,10 @@
-import { Link, useRouteError, useRouteLoaderData, useSubmit } from 'react-router-dom';
+import { Link, useRouteLoaderData, useSubmit } from 'react-router-dom';
 
 import classes from './EventItem.module.css';
 
 function EventItem({ event }) {
   const submit = useSubmit();
-  const token = useRouteLoaderData();
+  const token = useRouteLoaderData('root');
 
   function startDeleteHandler() {
     const proceed = window.confirm('Are you sure?');
